ff2f8ecc0ad49f04f2941b148397a8f02d626740
launebondo97 <munsibol9885@student.faytechcc.edu>
2026-09-12 00:39:41 -0500

    First Python Assignment
    Taking Input from User and Printing Output.

---
+++ p1lab1_Munsi Bondo Laurent.py
@@ -0,0 +1,7 @@
+# Laurent Munsi Bondo
+# 09.12.2026
+# p1lab1.py
+# My project consists of creating a Python program(input and print) using Visual Studio Code
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
